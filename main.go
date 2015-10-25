3ea2a91cc149705c2389a09b8dd8ab98edf43eec
Mike Vella <vellamike@gmail.com>
2015-10-25 01:35:34 +0000

    Tidy up and removed recursion

--- main.go
+++ main.go
@@ -19,6 +19,9 @@ package main
 // A If a type has all the correct function names, including signatures, then it satisfies an interface
 //   And can be passed to another method which takes that interface.
 //   Still not sure about the details of this but it sounds quite interesting.
+// Q how do maps work in go?
+//
+
 
 // UPDATE: I've decided that the 4x8 board representation is very unhelpful.
 // The 2x2x8 is better, that is to say, the board is represented as two player views.
@@ -30,6 +33,10 @@ package main
 // In some positions a decision can be made whether to go clockwise or counterclockwise.
 // Some sort of tree is going to be needed to keep track of decisions, OR we could not
 // implement this aspect of it for now.
+
+
+// Instruction Format: RCD (row, column, direction)
+// Example: 02C or 16A
 
 import "fmt"
 import "math/rand"
@@ -43,18 +50,8 @@ type Board struct {
 	player_2 player
 }
 
-func row_sum(intarray [8]int) (int) {
-	sum := 0
-	for i:= range intarray{
-		sum += intarray[i]
-	}
-	return sum
-}
-
-
-func (this player) naive_score() (score int) {
-	score = row_sum(this.positions[0]) + row_sum(this.positions[1]);
-	return
+func (this Board) is_birectional(player, row, column int) (bool){
+	return false
 }
 
 func random_position(num_seeds int) (player) {
@@ -79,13 +76,21 @@ func random_board(num_seeds int) (Board) {
 	return newboard
 }
 
+func next_position (current_row, current_column int) (int, int) {
+	switch {
+	case current_row == 0 && current_column < 7:
+		current_column += 1
+	case current_row == 0 && current_column == 7:
+		current_row = 1
+	case current_row == 1 && current_column > 0:
+		current_column -= 1
+	case current_row == 1 && current_column == 0:
+		current_row = 0
+	}
+	return current_row, current_column
+}
 
-func move(board Board, player_number int, row, column int) (Board) {
-	// Move counterclockwise.
-	// For now don't capture or re-move
-	var p *player
-	var p_op *player
-
+func players_from_name (player_number int, board *Board) (p, p_op *player) {
 	if player_number == 1 {
 		p = &board.player_1
 		p_op = &board.player_2
@@ -93,63 +98,52 @@ func move(board Board, player_number int, row, column int) (Board) {
 		p = &board.player_2
 		p_op = &board.player_1
 	}
-
-	fmt.Println("Initial position of player:")
-	fmt.Println(p)
-
-	fmt.Println("Initial position of opponent:")
-	fmt.Println(p_op)
-		
-
-	num_seeds := p.positions[row][column]
-
-	//Now do the clockwise moving
-	fmt.Println("Number of seeds player will move:")
-	fmt.Println(num_seeds)
-
-	current_row := row
-	current_column := column
-	p.positions[row][column] = 0 // empty the starting pit
-	for i := 0; i < num_seeds; i++{
-		// get the new current row and current column
-		switch {
-		case current_row == 0 && current_column < 7:
-			current_column += 1
-		case current_row == 0 && current_column == 7:
-			current_row = 1
-	        case current_row == 1 && current_column > 0:
-			current_column -= 1
-	        case current_row == 1 && current_column == 0:
-			current_row = 0
-		}
-		p.positions[current_row][current_column] += 1
-	}
-
-	oponent_column := 7 - current_column // oponent's column
-	fmt.Println("Opponent column targeted:")
-	fmt.Println(oponent_column)
-	oponent_row_0_seeds := p_op.positions[0][oponent_column]
-	oponent_row_1_seeds := p_op.positions[1][oponent_column]
-	if (oponent_row_0_seeds != 0 && oponent_row_1_seeds != 0 && current_row == 1) { // capture occurs
-		fmt.Println("Capture!")
-		p_op.positions[0][oponent_column] = 0
-		p_op.positions[1][oponent_column] = 0
-		captured_seeds := oponent_row_0_seeds + oponent_row_1_seeds
-		fmt.Println("Captured seeds:")
-		fmt.Println(captured_seeds)
-		p.positions[current_row][current_column] += captured_seeds
-		board = move(board, player_number, current_row, current_column)
-	}
-
-	return board
+	return p, p_op
 }
+func move(row, column int, direction string, board Board, player_number int) (terminal_board bool, final_board Board, next_instructions []string){
+	p, p_op := players_from_name(player_number, &board)
+
+	terminal_board = false // whether or not a leaf board has been reached
+	var ins []string // If not a leaf node, the instructions available from this board
+	for (terminal_board == false) || len(ins) != 0 {
+		num_seeds := p.positions[row][column]
+		p.positions[row][column] = 0 // empty the starting pit
+		for i := 0; i < num_seeds; i++{ //move the seeds, currently not using direction
+			row, column = next_position(row, column)
+			p.positions[row][column] += 1
+		} // need a separate method to perform the capture (lines below)
+		  // and a new one to decide what moves are available from that
+		 // from the new position, which in any case will be useful separately
+		oponent_column := 7 - column // oponent's column
+		oponent_row_0_seeds := p_op.positions[0][oponent_column]
+		oponent_row_1_seeds := p_op.positions[1][oponent_column]
+		if (oponent_row_0_seeds != 0 && oponent_row_1_seeds != 0 && row == 1) { // capture occurs
+			p_op.positions[0][oponent_column] = 0
+			p_op.positions[1][oponent_column] = 0
+			captured_seeds := oponent_row_0_seeds + oponent_row_1_seeds
+			p.positions[row][column] += captured_seeds
+			//if a capture occurs, need to evaluate whether there is more than one possible decision
+			if board.is_birectional(player_number, row, column) {
+				//this is never evaluated fo rnow, but will need to append new instructions
+			}
+		} else {terminal_board = true}
+	}
+	return terminal_board, board, ins
+}
+
+
+// methods needed:
+
+// 1. Return an array of all available layer-1 instructions
+// 2. Consume that array, returning a list of all final boards and corresponding instructions,
+//    including those for instructions which did not evaluate to leaf boards
 
 func main() {
 	fmt.Println("Instantiating a random board")
 	newboard := random_board(12)
-	moved_board := move(newboard, 1, 1, 2)
-	fmt.Println("Final position of player 1:")
-	fmt.Println(moved_board.player_1)
-	fmt.Println("Final position of opponent:")
-	fmt.Println(moved_board.player_2)
+	fmt.Println(newboard)
+	terminal_board, moved_board, instructions := move(1, 2, "A", newboard, 1)
+	fmt.Println(terminal_board)
+	fmt.Println(moved_board)
+	fmt.Println(instructions)
 }
