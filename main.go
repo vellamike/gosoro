ddfdc90c1624667d50bceaef50d4f1d02049ad96
Mike Vella <vellamike@gmail.com>
2015-10-27 23:37:56 +0000

    Added documentation and fixed play continuation bug

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 // TO DO:
-// 1. When computer and user move, if they capture, they can move again
-// 2. Computer should pick a move based on some goal, not just randomly
+// When computer and user move, if they capture, they should be able to move again
+// If capture does not finish on a bidirectional pit, no further instructions are
+// being returned
+
+// Computer is sometimes making illegal moves, these are listed as legal moves but then not correctly
+// Implemented - example 13A  which should lead to a capture (i.e it is a logical move) actually
+// being effected as 13C. The reason for this is not clear yet, possibly the boards and relevant
+// instructions are getting confused
+// idea: perhaps each instruction should contain the initial board, instruction, and final board
+// this would be a much more safe and OO approach.
+
 import "fmt"
 import "math/rand"
 import "bufio"
@@ -10,6 +19,7 @@ import "os"
 import "strconv"
 
 type player struct {
+	//Represents a player's territory in their frame of reference
 	positions [2][8]int
 }
 
@@ -33,8 +43,8 @@ type Instruction struct {
 }
 
 func (this Board) is_bidirectional(row, column int) bool {
+	//Whether clockwise and counterclockwise moves are allowed from this position
 	var bidir bool
-	//Omweso rules
 	if column == 0 || column == 1 || column == 6 || column == 7 {
 		bidir = true
 	} else {
@@ -44,6 +54,7 @@ func (this Board) is_bidirectional(row, column int) bool {
 }
 
 func (this Board) display() {
+	//Display the board to the screen from the computer's perspective
 	fmt.Println(reverse_array(this.player_2.positions[0]))
 	fmt.Println(reverse_array(this.player_2.positions[1]))
 	fmt.Println(this.player_1.positions[1])
@@ -51,6 +62,7 @@ func (this Board) display() {
 }
 
 func random_position(num_seeds int) player {
+	//choose a random pit
 	var p player
 
 	for i := 0; i < num_seeds; i++ {
@@ -64,6 +76,7 @@ func random_position(num_seeds int) player {
 }
 
 func random_board(num_seeds int) Board {
+	//Initialize a random board
 	var newboard Board
 
 	newboard.player_1 = random_position(num_seeds)
@@ -73,6 +86,7 @@ func random_board(num_seeds int) Board {
 }
 
 func next_position(current_row, current_column int, direction string) (int, int) {
+	//Based on the current position and direction, identify the next position
 	switch {
 	case current_row == 0 && current_column < 7 && direction == "A":
 		current_column += 1
@@ -95,6 +109,7 @@ func next_position(current_row, current_column int, direction string) (int, int)
 }
 
 func players_from_name(player_number int, board *Board) (p, p_op *player) {
+	//Retrun a player based on their identifier
 	if player_number == 1 {
 		p = &board.player_1
 		p_op = &board.player_2
@@ -106,6 +121,7 @@ func players_from_name(player_number int, board *Board) (p, p_op *player) {
 }
 
 func capture_possible(board Board, player_number, row, column int) bool {
+	//Returns whether landing in this pit would result in capturing oponent's seeds
 	_, p_op := players_from_name(player_number, &board)
 	opponent_column := 7 - column
 	opponent_row_0_seeds := p_op.positions[0][opponent_column]
@@ -125,15 +141,32 @@ func perform_capture(board Board, player_number, row, column int) (Board, []Inst
 
 	var next_instructions []Instruction
 
+	i1 := Instruction{row, column, "A", board}
+	next_instructions = []Instruction{i1}
+
 	if board.is_bidirectional(row, column) {
-		i1 := Instruction{row, column, "C", board}
-		i2 := Instruction{row, column, "A", board}
-		next_instructions = []Instruction{i1, i2}
+		i2 := Instruction{row, column, "C", board}
+		next_instructions = append(next_instructions, i2)
 	}
+	fmt.Println("Capture has been evaluated, the following instructions are being returned:")
+	fmt.Println(next_instructions)
 	return board, next_instructions
 }
 
+func reverse_array(arr [8]int) [8]int {
+	//Return the reverse of a size-8 array, handy for visualisation
+	num_elements := len(arr)
+	var reversed_array [8]int
+	for i := 0; i < num_elements; i++ {
+		reversed_array[i] = arr[num_elements-i-1]
+	}
+	return reversed_array
+}
+
 func execute_instruction(instruction Instruction, player_number int) (Board, []Instruction) {
+	// Execute the instruction (no decison on whether it is legal or not) and return
+	// Possible following instructions if there are any. Following instructions
+	// Result from captures
 	board := instruction.board
 	p, _ := players_from_name(player_number, &board)
 	current_row := instruction.row
@@ -141,11 +174,9 @@ func execute_instruction(instruction Instruction, player_number int) (Board, []I
 	current_direction := instruction.direction
 	var next_instructions []Instruction
 
-	// no while loops, just execute the instruction, including a capture, and return the board along with any next moves
-
-	num_seeds := p.positions[current_row][current_column]
-	p.positions[current_row][current_column] = 0 // empty the starting pit
-	for i := 0; i < num_seeds; i++ {             //move the seeds, currently not using direction
+	num_seeds := p.positions[current_row][current_column] //How many seeds will be moved
+	p.positions[current_row][current_column] = 0          // empty the starting pit
+	for i := 0; i < num_seeds; i++ {                      //move the seeds
 		current_row, current_column = next_position(current_row, current_column, current_direction)
 		p.positions[current_row][current_column] += 1
 	}
@@ -157,16 +188,10 @@ func execute_instruction(instruction Instruction, player_number int) (Board, []I
 	return board, next_instructions
 }
 
-func reverse_array(arr [8]int) [8]int {
-	num_elements := len(arr)
-	var reversed_array [8]int
-	for i := 0; i < num_elements; i++ {
-		reversed_array[i] = arr[num_elements-i-1]
-	}
-	return reversed_array
-}
-
 func pop_instruction_stack(stack *[][]Instruction) []Instruction {
+	// An instruction stack is slice of slices of instructions.
+	// This method returns a value off the stack, and removes that value
+	// From the stack
 	len_stack := len(*stack)
 	val := (*stack)[len_stack-1]
 	*stack = (*stack)[:len_stack-1]
@@ -186,29 +211,33 @@ func all_moves(board Board, player_number int) (boards []Board, instructions [][
 		}
 	}
 
-	var initial_instructions []Instruction
+	if len(coords) <= 0 {
+		fmt.Println("COMPUTER HAS BEEN DEFEATED")
+	}
+
+	var initial_instructions []Instruction // list of all initial instructions, not yet popualting the stack
 	for _, c := range coords {
-		instruction := Instruction{c.row, c.column, "A", board}
-		initial_instructions = append(initial_instructions, instruction)
-		if board.is_bidirectional(c.row, c.column) {
+		instruction := Instruction{c.row, c.column, "A", board}          // Perform the counterclockwise move
+		initial_instructions = append(initial_instructions, instruction) // Append instruction to initial instruction list
+		if board.is_bidirectional(c.row, c.column) {                     // If we can move counterclockwise from this position
 			instruction = Instruction{c.row, c.column, "C", board}
 			initial_instructions = append(initial_instructions, instruction)
 		}
 	}
 
-	var instructions_stack [][]Instruction
+	var instructions_stack [][]Instruction // list of lists of instructions
 
-	for _, i_instruction := range initial_instructions {
+	for _, i_instruction := range initial_instructions { // populate the instruction stack
 		instructions_stack = append(instructions_stack, []Instruction{i_instruction})
 	}
 
-	for len(instructions_stack) > 0 {
-		instruction_set := (pop_instruction_stack(&instructions_stack))
+	for len(instructions_stack) > 0 { // While the stack is not emtpy
+		instruction_set := (pop_instruction_stack(&instructions_stack)) // Get a list of instructions
 
-		instruction := instruction_set[len(instruction_set)-1]
-		b, i := execute_instruction(instruction, player_number)
+		instruction := instruction_set[len(instruction_set)-1]  // take the last instruction
+		b, i := execute_instruction(instruction, player_number) // Get the resulting instruction
 		if len(i) > 0 {
-			for _, ins := range i {
+			for _, ins := range i { // If more instructions, populate the instructions stack
 				new_instruction_set := append(instruction_set, ins)
 				instructions_stack = append(instructions_stack, new_instruction_set)
 			}
@@ -218,7 +247,7 @@ func all_moves(board Board, player_number int) (boards []Board, instructions [][
 		}
 	}
 
-	return boards, instructions
+	return boards, instructions // return the boards and full corresponding instruction sets
 }
 
 func user_move() (row, column int, direction string) {
@@ -241,27 +270,26 @@ func execute_user_move(board Board) Board {
 	return b
 }
 
-func score(board Board) (int){
+func score(board Board) int {
 	//returns the score
 	positions := board.player_1.positions
 	total := 0
-	for _,p := range positions{
-		for _,i := range p{
+	for _, p := range positions {
+		for _, i := range p {
 			total += i
 		}
 	}
 	return total
 }
 
-
 func computer_move(board Board) (Board, []Instruction) {
 	// Need to update so that the computer choses a move based on some optimality criterion
 	boards, instruction_sets := all_moves(board, 1)
 	max_index := 0
 	current_best_score := 0
-	for board_index, b := range boards{
+	for board_index, b := range boards {
 		s := score(b)
-		if s > current_best_score{
+		if s > current_best_score {
 			current_best_score = s
 			max_index = board_index
 		}
@@ -269,7 +297,7 @@ func computer_move(board Board) (Board, []Instruction) {
 
 	fmt.Println("I, the computer, chose this move:")
 	boards[max_index].display()
-	return boards[max_index], instruction_sets[max_index]
+	return boards[max_index], instruction_sets[max_index] // return final board and corresponding instruction sets
 }
 
 func main() {
@@ -279,8 +307,7 @@ func main() {
 	fmt.Println("Play now begins...")
 	for true {
 		board1, instructions := computer_move(newboard)
-		fmt.Println(instructions[0].row, instructions[0].column, instructions[0].direction)
-
+		fmt.Println(instructions)
 		newboard = execute_user_move(board1)
 		newboard.display()
 	}
