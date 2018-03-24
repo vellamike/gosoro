837feca25fafd5f87f883c1850a236d3868039eb
Mike Vella <mike.vella@nanoporetech.com>
2018-03-24 21:38:57 +0000

    Got rid of some stuff which was no longer needed

--- gamecontrollers/gamecontrollers.go
+++ gamecontrollers/gamecontrollers.go
@@ -19,16 +19,6 @@ type gamecontroller struct {
 	NextPlayer int
 }
 
-// TODO: Still needed?
-func (self gamecontroller) LastUserPosition() ds.Coord {
-	return self.board.Player_1.LastPosition
-}
-
-// TODO: Still Needed?
-func (self gamecontroller) LastComputerPosition() ds.Coord {
-	return self.board.Player_2.LastPosition
-}
-
 func NewGameController(generator func() ds.Board, ai ai.AI, ruleset rulesets.IgisoroRuleSet, evaluator evaluators.Evaluator) *gamecontroller {
 	board := generator()
 	b := gamecontroller{board,
@@ -54,33 +44,6 @@ func capture_possible(board ds.Board, player_number, row, column int) bool {
 	opponent_row_1_seeds := p_op.Positions[1][opponent_column]
 	return (opponent_row_0_seeds != 0 && opponent_row_1_seeds != 0 && row == 1)
 }
-
-// TODO: No longer needed?
-func user_move() []ds.Move { // Takes user input as a string and returns a slice of Moves
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your move: ")
-	text, _ := reader.ReadString('\n')
-	t := string(text)
-
-	var rows, columns []int
-	var actions []string
-
-	var moves []ds.Move
-
-	for i := 1; i < len(t)/3+1; i++ {
-		row, _ := strconv.Atoi(t[i*3-3 : i*3-2])
-		column, _ := strconv.Atoi(t[i*3-2 : i*3-1])
-		action := t[i*3-1 : i*3]
-		rows = append(rows, row)
-		columns = append(columns, column)
-		actions = append(actions, action)
-
-		moves = append(moves, ds.Move{row, column, action})
-
-	}
-	return moves
-}
-
 
 func (gc *gamecontroller) UserMove() {
 	// Ask the user for a move, create an instruction from their reply
@@ -140,28 +103,3 @@ func (gc *gamecontroller) ApplyMove(move ds.Move, user int){
 func (gc * gamecontroller) ComputerMove(){
 
 }
-
-
-
-
-
-//func (gc *gamecontroller) ComputerMove() {
-//	// Step 1: Ask the AI for the best instruction
-//
-//	moveSequence := gc.ai.BestInstruction(gc.board, gc.ruleset, gc.evaluator)
-//	fmt.Println("Computer's response:")
-//	fmt.Println(moveSequence)
-//
-//	// Step 2: Apply the instruction
-//
-//	gc.board = gc.board.ExecuteMoveSequence(moveSequence, 2)
-//
-//	fmt.Println("Board after computer's response:")
-//	gc.board.Display()
-//
-//	fmt.Println("Now computer performing a capture")
-//
-//	fmt.Println("Board after capture performed:")
-//	gc.board.Display()
-//
-//}
